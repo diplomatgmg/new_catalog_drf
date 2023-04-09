59d811a7ef97ab37338d85210fa7dc624bfaad89
Кирилл Попов <diplomatgmg@gmail.com>
2023-04-09 21:58:48 +0300

    Улучшена фильтрация для процессора

--- apps/product/filters.py
+++ apps/product/filters.py
@@ -3,7 +3,59 @@ import django_filters
 from apps.product.models import CPU
 
 
-class CPUFilter(django_filters.FilterSet):
+class BaseFilter(django_filters.FilterSet):
+    multiple_choices = []
+    range_choices = []
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name in self.multiple_choices:
+            self.filters[field_name] = django_filters.MultipleChoiceFilter(
+                field_name=field_name
+            )
+            self.filters[field_name].extra["choices"] = self.get_multiple_choices(
+                field_name
+            )
+        for field_name in self.range_choices:
+            self.filters[field_name] = django_filters.RangeFilter(field_name=field_name)
+
+    def get_multiple_choices(self, field):
+        values_list = (
+            getattr(model, field)
+            for model in self.queryset
+            if getattr(model, field) or getattr(model, field) is False
+        )
+        return ((value, value) for value in sorted(set(values_list)))
+
+
+class CPUFilter(BaseFilter):
     class Meta:
         model = CPU
-        fields = '__all__'
+        fields = "__all__"
+        exclude = ["category", "slug"]
+
+    multiple_choices = [
+        "family",
+        "model",
+        "year",
+        "segment",
+        "socket",
+        "unlocked_multiplier",
+        "architecture",
+        "technology",
+        "integrated_graphics",
+        "memory_controller",
+        "pcie",
+    ]
+
+    range_choices = [
+        "cores",
+        "threads",
+        "base_clock",
+        "boost_clock",
+        "tdp",
+        "max_temperature",
+        "l1_cache",
+        "l2_cache",
+        "l3_cache",
+    ]
--- apps/product/views.py
+++ apps/product/views.py
@@ -1,5 +1,6 @@
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import pagination, viewsets, mixins, filters as rest_filters
+import django_filters
+from rest_framework import pagination
+from rest_framework.viewsets import ModelViewSet
 
 from apps.product.filters import CPUFilter
 from apps.product.models import CPU, GPU
@@ -10,20 +11,15 @@ class ModelPagination(pagination.PageNumberPagination):
     page_size = 15
 
 
-class BaseModelViewSet(
-    mixins.RetrieveModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet
-):
+class BaseModelViewSet(ModelViewSet):
     pagination_class = ModelPagination
-    filter_backends = [
-        DjangoFilterBackend,
-    ]
-    ordering_fields = ["slug", "year"]
 
 
 class CPUModelViewSet(BaseModelViewSet):
     queryset = CPU.objects.all()
     serializer_class = CPUSerializer
     filterset_class = CPUFilter
+
 
 class GPUModelViewSet(BaseModelViewSet):
     queryset = GPU.objects.all()
